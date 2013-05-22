9dfb0bf42c800b8ea77d23755b740c79976b333b
Michael Jackson <majgis@gmail.com>
2013-05-22 01:51:08 -0600

    Paste code from phonecat example,  reformat code for readability

--- app/scripts/controllers/PhoneList.js
+++ app/scripts/controllers/PhoneList.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('ngBbApp')
-  .controller('PhoneListCtrl', ["$scope", function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  }]);
+    .controller('PhoneListCtrl', [
+        "$scope",
+        function ($scope) {
+            $scope.phones = Phone.query();
+            $scope.orderProp = 'age';
+        }
+    ]);
