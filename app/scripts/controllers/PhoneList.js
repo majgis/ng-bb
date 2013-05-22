45ee462ed9ac46212f2fe99eb6cb252b0aaa3169
Michael Jackson <majgis@gmail.com>
2013-05-22 01:47:34 -0600

    Since angular infers the controller's dependencies from the names of arguments to the controller's constructor function, if you were to minify the JavaScript code for PhoneListCtrl controller, all of its function arguments would be minified as well, and the dependency injector would not be able to identify services correctly.
    To avoid minification issues,  use the bracket notation which wraps the function to be injected into an array of strings (representing the dependency names) followed by the function to be injected.

--- app/scripts/controllers/PhoneList.js
+++ app/scripts/controllers/PhoneList.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('ngBbApp')
-  .controller('PhoneListCtrl', function ($scope) {
+  .controller('PhoneListCtrl', ["$scope", function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+  }]);
