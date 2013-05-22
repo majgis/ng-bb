9db698fd72ca00664842ff07f46f525421262e2d
Michael Jackson <majgis@gmail.com>
2013-05-22 01:35:16 -0600

    The following command was executed:   sudo yo angular:controller PhoneList

---
+++ app/scripts/controllers/PhoneList.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngBbApp')
+  .controller('PhoneListCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
