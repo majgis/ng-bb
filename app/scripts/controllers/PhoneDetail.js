c45a7707d2b8fa727506ea0aa26fb6ef6a1f44e0
Michael Jackson <majgis@gmail.com>
2013-05-22 02:00:28 -0600

    Paste code from phonecat example,  reformat code for readability

--- app/scripts/controllers/PhoneDetail.js
+++ app/scripts/controllers/PhoneDetail.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('ngBbApp')
-  .controller('PhoneDetailCtrl', ["$scope", function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  }]);
+    .controller('PhoneDetailCtrl', [
+        "$scope", "$routeParams", "Phone",
+        function ($scope, $routeParams, Phone) {
+
+            $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function (phone) {
+                $scope.mainImageUrl = phone.images[0];
+            });
+
+            $scope.setImage = function (imageUrl) {
+                $scope.mainImageUrl = imageUrl;
+            }
+
+        }
+    ]);
